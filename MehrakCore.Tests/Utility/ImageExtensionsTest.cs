41bebd06bc021608b7de94ef003c9f44e3eb48cb
GamerYuan <f2pplayer0801@gmail.com>
2025-05-07 15:17:00 +0800

    refactor: better memory management

--- MehrakCore.Tests/Utility/ImageExtensionsTest.cs
+++ MehrakCore.Tests/Utility/ImageExtensionsTest.cs
@@ -13,32 +13,32 @@ public class ImageExtensionsTest
     [Test]
     public void StandardizeImageSize_WithLargeSquareImage_ReturnsResizedImage()
     {
-        Image<Rgba32> inputImage = new Image<Rgba32>(2000, 2000);
+        using Image<Rgba32> inputImage = new Image<Rgba32>(2000, 2000);
         var output = ImageExtensions.StandardizeImageSize(inputImage, 1000);
-        Assert.That(inputImage.Size, Is.EqualTo(new Size(1000, 1000)));
+        Assert.That(output.Size, Is.EqualTo(new Size(1000, 1000)));
     }
 
     [Test]
     public void StandardizeImageSize_WithSmallSquareImage_ReturnsResizedImage()
     {
-        Image<Rgba32> inputImage = new Image<Rgba32>(500, 500);
+        using Image<Rgba32> inputImage = new Image<Rgba32>(500, 500);
         var output = ImageExtensions.StandardizeImageSize(inputImage, 1000);
-        Assert.That(inputImage.Size, Is.EqualTo(new Size(1000, 1000)));
+        Assert.That(output.Size, Is.EqualTo(new Size(1000, 1000)));
     }
 
     [Test]
     public void StandardizeImageSize_WithLargeRectangularImage_ReturnsResizedImage()
     {
-        Image<Rgba32> inputImage = new Image<Rgba32>(2000, 1000);
+        using Image<Rgba32> inputImage = new Image<Rgba32>(2000, 1000);
         var output = ImageExtensions.StandardizeImageSize(inputImage, 1000);
-        Assert.That(inputImage.Size, Is.EqualTo(new Size(1000, 1000)));
+        Assert.That(output.Size, Is.EqualTo(new Size(1000, 1000)));
     }
 
     [Test]
     public void StandardizeImageSize_WithSmallRectangularImage_ReturnsResizedImage()
     {
-        Image<Rgba32> inputImage = new Image<Rgba32>(500, 1000);
+        using Image<Rgba32> inputImage = new Image<Rgba32>(500, 1000);
         var output = ImageExtensions.StandardizeImageSize(inputImage, 1000);
-        Assert.That(inputImage.Size, Is.EqualTo(new Size(1000, 1000)));
+        Assert.That(output.Size, Is.EqualTo(new Size(1000, 1000)));
     }
 }
