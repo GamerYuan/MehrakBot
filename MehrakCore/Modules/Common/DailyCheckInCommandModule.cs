3f7c0aa1d78147b1bed14268719c08758f6ed970
GamerYuan <f2pplayer0801@gmail.com>
2025-05-29 10:50:22 +0800

    feat: rate limit for daily check in command

--- MehrakCore/Modules/Common/DailyCheckInCommandModule.cs
+++ MehrakCore/Modules/Common/DailyCheckInCommandModule.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using MehrakCore.Services.Commands;
+using MehrakCore.Services.Common;
 using Microsoft.Extensions.Logging;
 using NetCord;
 using NetCord.Rest;
@@ -13,16 +14,18 @@ namespace MehrakCore.Modules.Common;
 public class DailyCheckInCommandModule : ApplicationCommandModule<ApplicationCommandContext>, ICommandModule
 {
     private readonly IDailyCheckInCommandService<DailyCheckInCommandModule> m_Executor;
+    private readonly CommandRateLimitService m_CommandRateLimitService;
     private readonly ILogger<DailyCheckInCommandModule> m_Logger;
 
     public DailyCheckInCommandModule(
         IDailyCheckInCommandService<DailyCheckInCommandModule> executor,
+        CommandRateLimitService commandRateLimitService,
         ILogger<DailyCheckInCommandModule> logger)
     {
         m_Executor = executor;
+        m_CommandRateLimitService = commandRateLimitService;
         m_Logger = logger;
     }
-
     [SlashCommand("checkin", "Perform HoYoLAB Daily Check-In")]
     public async Task DailyCheckInCommand(
         [SlashCommandParameter(Name = "profile", Description = "Profile ID (Defaults to 1)")]
@@ -30,6 +33,9 @@ public class DailyCheckInCommandModule : ApplicationCommandModule<ApplicationCom
     {
         try
         {
+            // Check rate limit first
+            if (!await ValidateRateLimitAsync()) return;
+
             // Set the context for the executor
             m_Executor.Context = Context;
 
@@ -44,6 +50,20 @@ public class DailyCheckInCommandModule : ApplicationCommandModule<ApplicationCom
                     .WithContent("An error occurred while processing your request. Please try again later.")
                     .WithFlags(MessageFlags.Ephemeral)));
         }
+    }
+
+    private async Task<bool> ValidateRateLimitAsync()
+    {
+        if (await m_CommandRateLimitService.IsRateLimitedAsync(Context.Interaction.User.Id))
+        {
+            await Context.Interaction.SendResponseAsync(InteractionCallback.Message(
+                new InteractionMessageProperties().WithContent("Used command too frequent! Please try again later")
+                    .WithFlags(MessageFlags.Ephemeral)));
+            return false;
+        }
+
+        await m_CommandRateLimitService.SetRateLimitAsync(Context.Interaction.User.Id);
+        return true;
     }
 
     public static string GetHelpString()
