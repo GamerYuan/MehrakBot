8566ec90c1423d1d5825e37346975351cca39905
GamerYuan <f2pplayer0801@gmail.com>
2025-06-12 13:37:08 +0800

    chore: bump packages

---
+++ MehrakCore/Modules/Common/HealthCommandModule.cs
@@ -0,0 +1,11 @@
+﻿#region
+
+using NetCord.Services.ApplicationCommands;
+
+#endregion
+
+namespace MehrakCore.Modules.Common;
+
+public class HealthCommandModule : ApplicationCommandModule<ApplicationCommandContext>
+{
+}
