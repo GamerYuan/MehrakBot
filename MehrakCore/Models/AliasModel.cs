d81bdc660208519f3c85761a760e372b97aec181
GamerYuan <f2pplayer0801@gmail.com>
2025-07-01 22:42:05 +0800

    feat: add alias dto

---
+++ MehrakCore/Models/AliasModel.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+#endregion
+
+namespace MehrakCore.Models;
+
+public class AliasModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+
+    [BsonElement("game")] public GameName Game { get; set; }
+
+    [BsonElement("alias")] public required Dictionary<string, string> Alias { get; set; }
+}
