faaf60d0938dbba2d6391ed1d0b84f3c38f18bc8
GamerYuan <f2pplayer0801@gmail.com>
2025-07-01 14:16:22 +0800

    feat: genshin autocomplete service

---
+++ MehrakCore/Services/Commands/Genshin/Character/GenshinCharacterAutocompleteService.cs
@@ -0,0 +1,28 @@
+#region
+
+using MehrakCore.Models;
+using MehrakCore.Services.Common;
+
+#endregion
+
+namespace MehrakCore.Services.Commands.Genshin.Character;
+
+public class GenshinCharacterAutocompleteService
+{
+    private readonly ICharacterCacheService m_CharacterCacheService;
+    private const int Limit = 25;
+
+    public GenshinCharacterAutocompleteService(ICharacterCacheService characterCacheService)
+    {
+        m_CharacterCacheService = characterCacheService;
+    }
+
+    public IReadOnlyList<string> FindCharacter(string query)
+    {
+        var characterNames = m_CharacterCacheService.GetCharacters(GameName.Genshin);
+        return characterNames
+            .Where(x => x.Contains(query, StringComparison.InvariantCultureIgnoreCase))
+            .Take(Limit)
+            .ToList();
+    }
+}
