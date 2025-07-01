ea31494f9705d986e68816771efaa9332c6f5fe3
GamerYuan <f2pplayer0801@gmail.com>
2025-07-01 14:16:01 +0800

    refactor: hsr autocomplete service use new classes

--- MehrakCore/Services/Commands/Hsr/Character/HsrCharacterAutocompleteService.cs
+++ MehrakCore/Services/Commands/Hsr/Character/HsrCharacterAutocompleteService.cs
@@ -1,21 +1,28 @@
-﻿namespace MehrakCore.Services.Commands.Hsr.Character;
+﻿#region
+
+using MehrakCore.Models;
+using MehrakCore.Services.Common;
+
+#endregion
+
+namespace MehrakCore.Services.Commands.Hsr.Character;
 
 public class HsrCharacterAutocompleteService
 {
-    private List<string> CharacterNames { get; }
-
+    private readonly ICharacterCacheService m_CharacterCacheService;
     private const int Limit = 25;
 
-    public HsrCharacterAutocompleteService()
+    public HsrCharacterAutocompleteService(ICharacterCacheService characterCacheService)
     {
-        CharacterNames =
-            File.ReadAllLines(Path.Combine(AppContext.BaseDirectory, "Assets", "Hsr", "hsr_character_list.txt"))
-                .OrderBy(x => x).ToList();
+        m_CharacterCacheService = characterCacheService;
     }
 
     public IReadOnlyList<string> FindCharacter(string query)
     {
-        return CharacterNames.Where(x => x.Contains(query, StringComparison.InvariantCultureIgnoreCase)).Take(Limit)
+        var characterNames = m_CharacterCacheService.GetCharacters(GameName.HonkaiStarRail);
+        return characterNames
+            .Where(x => x.Contains(query, StringComparison.InvariantCultureIgnoreCase))
+            .Take(Limit)
             .ToList();
     }
 }
