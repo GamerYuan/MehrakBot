6f108c33c39d3add5f5fdfdd631a95df525ef6c6
GamerYuan <f2pplayer0801@gmail.com>
2025-08-17 08:49:03 +0800

    feat: add genshin stats name to file name constants

--- MehrakCore/Constants/FileNameFormat.cs
+++ MehrakCore/Constants/FileNameFormat.cs
@@ -6,22 +6,27 @@
 public static class FileNameFormat
 {
     /// <summary>
-    /// Genshin file name format, where {0} is the character ID or other identifier.
+    /// Genshin file name format, where {0} is the character ID or other identifier
     /// </summary>
     public const string GenshinFileName = "genshin_{0}";
 
     /// <summary>
-    /// Genshin avatar file name format, where {0} is the avatar ID.
+    /// Genshin avatar file name format, where {0} is the avatar ID
     /// </summary>
     public const string GenshinAvatarName = "genshin_avatar_{0}";
 
     /// <summary>
-    /// Genshin side avatar file name format, where {0} is the avatar ID.
+    /// Genshin side avatar file name format, where {0} is the avatar ID
     /// </summary>
     public const string GenshinSideAvatarName = "genshin_side_avatar_{0}";
 
     /// <summary>
-    /// Genshin skill file name format, where {0} is the character ID and {1} is the skill ID.
+    /// Genshin skill file name format, where {0} is the character ID and {1} is the skill ID
     /// </summary>
     public const string GenshinSkillName = "genshin_{0}_{1}";
+
+    /// <summary>
+    /// Genshin stats file name format, where {0} is the stat ID
+    /// </summary>
+    public const string GenshinStatsName = "genshin_stats_{0}";
 }
