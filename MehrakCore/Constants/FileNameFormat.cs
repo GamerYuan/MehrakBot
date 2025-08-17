83776ea88499eec8f6ea60b5d38a7b0aced34d4b
GamerYuan <f2pplayer0801@gmail.com>
2025-08-17 08:33:19 +0800

    feat: add file name format constants

---
+++ MehrakCore/Constants/FileNameFormat.cs
@@ -0,0 +1,27 @@
+﻿namespace MehrakCore.Constants;
+
+/// <summary>
+/// File name format string constants
+/// </summary>
+public static class FileNameFormat
+{
+    /// <summary>
+    /// Genshin file name format, where {0} is the character ID or other identifier.
+    /// </summary>
+    public const string GenshinFileName = "genshin_{0}";
+
+    /// <summary>
+    /// Genshin avatar file name format, where {0} is the avatar ID.
+    /// </summary>
+    public const string GenshinAvatarName = "genshin_avatar_{0}";
+
+    /// <summary>
+    /// Genshin side avatar file name format, where {0} is the avatar ID.
+    /// </summary>
+    public const string GenshinSideAvatarName = "genshin_side_avatar_{0}";
+
+    /// <summary>
+    /// Genshin skill file name format, where {0} is the character ID and {1} is the skill ID.
+    /// </summary>
+    public const string GenshinSkillName = "genshin_{0}_{1}";
+}
