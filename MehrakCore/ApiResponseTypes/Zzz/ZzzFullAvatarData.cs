4a70850181a5226ef9a71f784454fdc4dcca312e
GamerYuan <f2pplayer0801@gmail.com>
2025-08-18 11:48:23 +0800

    feat: add zzz character api response types

---
+++ MehrakCore/ApiResponseTypes/Zzz/ZzzFullAvatarData.cs
@@ -0,0 +1,279 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MehrakCore.ApiResponseTypes.Zzz;
+
+public class ZzzAvatarData
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("name_mi18n")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("full_name_mi18n")]
+    public required string FullName { get; set; }
+
+    [JsonPropertyName("element_type")]
+    public int ElementType { get; set; }
+
+    [JsonPropertyName("camp_name_mi18n")]
+    public required string CampName { get; set; }
+
+    [JsonPropertyName("avatar_profession")]
+    public int AvatarProfession { get; set; }
+
+    [JsonPropertyName("rarity")]
+    public required string Rarity { get; set; }
+
+    [JsonPropertyName("group_icon_path")]
+    public required string GroupIconPath { get; set; }
+
+    [JsonPropertyName("hollow_icon_path")]
+    public required string HollowIconPath { get; set; }
+
+    [JsonPropertyName("equip")]
+    public required List<Equip> Equip { get; set; }
+
+    [JsonPropertyName("weapon")]
+    public Weapon? Weapon { get; set; }
+
+    [JsonPropertyName("properties")]
+    public required List<Property> Properties { get; set; }
+
+    [JsonPropertyName("skills")]
+    public required List<Skill> Skills { get; set; }
+
+    [JsonPropertyName("rank")]
+    public int Rank { get; set; }
+
+    [JsonPropertyName("ranks")]
+    public required List<Rank> Ranks { get; set; }
+
+    [JsonPropertyName("role_vertical_painting_url")]
+    public required string RoleVerticalPaintingUrl { get; set; }
+
+    /// <summary>
+    /// Hex color code for background color
+    /// </summary>
+    [JsonPropertyName("vertical_painting_color")]
+    public required string VerticalPaintingColor { get; set; }
+
+    [JsonPropertyName("sub_element_type")]
+    public int SubElementType { get; set; }
+
+    [JsonPropertyName("role_square_url")]
+    public required string RoleSquareUrl { get; set; }
+
+    [JsonPropertyName("awaken_state")]
+    public required string AwakenState { get; set; }
+}
+
+public class ZzzFullAvatarData
+{
+    [JsonPropertyName("avatar_list")]
+    public required List<ZzzAvatarData> AvatarList { get; set; }
+
+    [JsonPropertyName("equip_wiki")]
+    public Dictionary<string, string>? EquipWiki { get; set; }
+
+    [JsonPropertyName("weapon_wiki")]
+    public Dictionary<string, string>? WeaponWiki { get; set; }
+
+    [JsonPropertyName("avatar_wiki")]
+    public Dictionary<string, string>? AvatarWiki { get; set; }
+
+    [JsonPropertyName("strategy_wiki")]
+    public Dictionary<string, string>? StrategyWiki { get; set; }
+}
+
+public class Equip
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("icon")]
+    public required string Icon { get; set; }
+
+    [JsonPropertyName("rarity")]
+    public required string Rarity { get; set; }
+
+    [JsonPropertyName("properties")]
+    public required List<Property> Properties { get; set; }
+
+    [JsonPropertyName("main_properties")]
+    public required List<MainProperty> MainProperties { get; set; }
+
+    [JsonPropertyName("equip_suit")]
+    public required EquipSuit EquipSuit { get; set; }
+
+    [JsonPropertyName("equipment_type")]
+    public int EquipmentType { get; set; }
+
+    [JsonPropertyName("invalid_property_cnt")]
+    public int InvalidPropertyCnt { get; set; }
+
+    [JsonPropertyName("all_hit")]
+    public bool AllHit { get; set; }
+}
+
+/// <summary>
+/// Disk Drive set details
+/// </summary>
+public class EquipSuit
+{
+    [JsonPropertyName("suit_id")]
+    public int SuitId { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("own")]
+    public int Own { get; set; }
+
+    [JsonPropertyName("desc1")]
+    public required string Desc1 { get; set; }
+
+    [JsonPropertyName("desc2")]
+    public required string Desc2 { get; set; }
+}
+
+public class SkillDetails
+{
+    [JsonPropertyName("title")]
+    public required string Title { get; set; }
+
+    [JsonPropertyName("text")]
+    public required string Text { get; set; }
+
+    [JsonPropertyName("awaken")]
+    public bool Awaken { get; set; }
+}
+
+public class MainProperty
+{
+    [JsonPropertyName("property_name")]
+    public required string PropertyName { get; set; }
+
+    [JsonPropertyName("property_id")]
+    public int PropertyId { get; set; }
+
+    [JsonPropertyName("base")]
+    public required string Base { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("valid")]
+    public bool Valid { get; set; }
+
+    [JsonPropertyName("system_id")]
+    public int SystemId { get; set; }
+
+    [JsonPropertyName("add")]
+    public int Add { get; set; }
+}
+
+public class Property
+{
+    [JsonPropertyName("property_name")]
+    public required string PropertyName { get; set; }
+
+    [JsonPropertyName("property_id")]
+    public int PropertyId { get; set; }
+
+    [JsonPropertyName("base")]
+    public required string Base { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("valid")]
+    public bool Valid { get; set; }
+
+    [JsonPropertyName("system_id")]
+    public int SystemId { get; set; }
+
+    [JsonPropertyName("add")]
+    public int Add { get; set; }
+
+    [JsonPropertyName("final")]
+    public required string Final { get; set; }
+}
+
+public class Rank
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("desc")]
+    public required string Desc { get; set; }
+
+    [JsonPropertyName("pos")]
+    public int Pos { get; set; }
+
+    [JsonPropertyName("is_unlocked")]
+    public bool IsUnlocked { get; set; }
+}
+
+public class Skill
+{
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("skill_type")]
+    public int SkillType { get; set; }
+
+    [JsonPropertyName("items")]
+    public required List<SkillDetails> Items { get; set; }
+
+    [JsonPropertyName("awaken_state")]
+    public required string AwakenState { get; set; }
+}
+
+public class Weapon
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("star")]
+    public int Star { get; set; }
+
+    [JsonPropertyName("icon")]
+    public required string Icon { get; set; }
+
+    [JsonPropertyName("rarity")]
+    public required string Rarity { get; set; }
+
+    [JsonPropertyName("properties")]
+    public required List<Property> Properties { get; set; }
+
+    [JsonPropertyName("main_properties")]
+    public required List<MainProperty> MainProperties { get; set; }
+
+    [JsonPropertyName("talent_title")]
+    public required string TalentTitle { get; set; }
+
+    [JsonPropertyName("talent_content")]
+    public required string TalentContent { get; set; }
+
+    [JsonPropertyName("profession")]
+    public int Profession { get; set; }
+}
