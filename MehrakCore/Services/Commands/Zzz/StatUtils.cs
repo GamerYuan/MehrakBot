6df7fcc9a4423b67746692b1c2bddffaceb51f38
GamerYuan <f2pplayer0801@gmail.com>
2025-08-19 10:39:03 +0800

    refactor: update zzz stat utils with new string format and aggresive inlining

--- MehrakCore/Services/Commands/Zzz/StatUtils.cs
+++ MehrakCore/Services/Commands/Zzz/StatUtils.cs
@@ -1,9 +1,12 @@
-﻿namespace MehrakCore.Services.Commands.Zzz;
+﻿using System.Runtime.CompilerServices;
+
+namespace MehrakCore.Services.Commands.Zzz;
 
 internal static class StatUtils
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static string GetStatAssetName(string statName)
     {
-        return statName.Replace(' ', '_').TrimEnd().ToLowerInvariant().Insert(0, "zzz_stats_");
+        return statName.Replace(' ', '_').TrimEnd().ToLowerInvariant();
     }
 }
