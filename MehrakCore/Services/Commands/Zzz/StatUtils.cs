2cc4c8e3f0a5cbeb1593408dfe2fb2836e18293e
GamerYuan <f2pplayer0801@gmail.com>
2025-08-19 08:57:22 +0800

    feat: add zzz stat utils

---
+++ MehrakCore/Services/Commands/Zzz/StatUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace MehrakCore.Services.Commands.Zzz;
+
+internal static class StatUtils
+{
+    internal static string GetStatAssetName(string statName)
+    {
+        return statName.Replace(' ', '_').TrimEnd().ToLowerInvariant().Insert(0, "zzz_stats_");
+    }
+}
