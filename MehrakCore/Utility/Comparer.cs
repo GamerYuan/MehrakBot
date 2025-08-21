3e4cfe39fee4899f4de70ae53a885345b2ee368e
GamerYuan <f2pplayer0801@gmail.com>
2025-08-21 15:33:48 +0800

    feat: add case insensitive character comparer

---
+++ MehrakCore/Utility/Comparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MehrakCore.Utility;
+
+public class CaseInsensitiveCharComparer : IEqualityComparer<char>
+{
+    public static CaseInsensitiveCharComparer Instance { get; } = new CaseInsensitiveCharComparer();
+
+    public bool Equals(char x, char y)
+    {
+        return char.ToLowerInvariant(x) == char.ToLowerInvariant(y);
+    }
+
+    public int GetHashCode([DisallowNull] char obj)
+    {
+        return char.ToLowerInvariant(obj).GetHashCode();
+    }
+}
