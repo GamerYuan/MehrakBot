537b27f2197c50253280280d95c6647eafa89217
GamerYuan <f2pplayer0801@gmail.com>
2025-08-21 15:33:27 +0800

    feat: add attribute mapping to stat utils

--- MehrakCore/Services/Commands/Zzz/StatUtils.cs
+++ MehrakCore/Services/Commands/Zzz/StatUtils.cs
@@ -1,12 +1,42 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace MehrakCore.Services.Commands.Zzz;
 
-internal static class StatUtils
+internal static partial class StatUtils
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static string GetStatAssetName(string statName)
     {
-        return statName.Replace(' ', '_').TrimEnd().ToLowerInvariant();
+        return DamageBonusRegex().Replace(statName, "").Replace(' ', '_').TrimEnd().ToLowerInvariant();
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static string GetSpecialAttributeName(int subElementId)
+    {
+        return subElementId switch
+        {
+            1 => "frost",
+            2 => "auricink",
+            _ => throw new ArgumentOutOfRangeException(nameof(subElementId),
+                subElementId, null)
+        };
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static string GetElementNameFromId(int elementId)
+    {
+        return elementId switch
+        {
+            200 => "physical",
+            201 => "fire",
+            202 => "ice",
+            203 => "electric",
+            205 => "ether",
+            _ => throw new ArgumentOutOfRangeException(nameof(elementId), elementId, null)
+        };
+    }
+
+    [GeneratedRegex(@"\sDMG\sBonus")]
+    private static partial Regex DamageBonusRegex();
 }
