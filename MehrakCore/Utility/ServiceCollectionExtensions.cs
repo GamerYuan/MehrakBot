d26cbcfbf686c58a31f4d2c22693c6c18ca16be8
GamerYuan <f2pplayer0801@gmail.com>
2025-08-26 21:38:46 +0800

    feat: add async initializable and initialization service

---
+++ MehrakCore/Utility/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MehrakCore.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MehrakCore.Utility;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterAsyncInitializable<T>(
+        this IServiceCollection services)
+        where T : class, IAsyncInitializable
+    {
+        services.AddScoped<IAsyncInitializable>(sp => sp.GetRequiredService<T>());
+        return services;
+    }
+}
