b683e59f3c5d5dfa3619f94ca28f3181d9dce372
GamerYuan <f2pplayer0801@gmail.com>
2025-09-09 18:56:24 +0800

    feat: add zzz defense api service

---
+++ MehrakCore/Services/Commands/Zzz/Defense/ZzzDefenseApiService.cs
@@ -0,0 +1,49 @@
+﻿using MehrakCore.ApiResponseTypes;
+using MehrakCore.ApiResponseTypes.Zzz;
+using MehrakCore.Constants;
+using MehrakCore.Models;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+
+namespace MehrakCore.Services.Commands.Zzz.Defense;
+
+internal class ZzzDefenseApiService : IApiService<ZzzDefenseCommandExecutor>
+{
+    private const string ApiEndpoint = "/event/game_record_zzz/api/zzz/challenge";
+
+    private readonly IHttpClientFactory m_HttpClientFactory;
+    private readonly ILogger<ZzzDefenseApiService> m_Logger;
+
+    public ZzzDefenseApiService(IHttpClientFactory httpClientFactory, ILogger<ZzzDefenseApiService> logger)
+    {
+        m_HttpClientFactory = httpClientFactory;
+        m_Logger = logger;
+    }
+
+    public async ValueTask<ZzzDefenseData> GetDefenseDataAsync(string ltoken, ulong ltuid, string gameUid, string region)
+    {
+        HttpClient client = m_HttpClientFactory.CreateClient("Default");
+        HttpRequestMessage request = new(HttpMethod.Get,
+            $"{HoYoLabDomains.PublicApi}{ApiEndpoint}?server={region}&role_id={gameUid}&schedule_type=1");
+        request.Headers.Add("Cookie", $"ltoken_v2={ltoken}; ltuid_v2={ltuid};");
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            m_Logger.LogError("Failed to fetch Zzz Defense data. Status Code: {StatusCode}", response.StatusCode);
+            throw new HttpRequestException($"Failed to fetch Zzz Defense data. Status Code: {response.StatusCode}");
+        }
+
+        ApiResponse<ZzzDefenseData>? json =
+            await response.Content.ReadFromJsonAsync<ApiResponse<ZzzDefenseData>>();
+
+        if (json == null || json.Retcode != 0)
+        {
+            m_Logger.LogError("Error in API response: Retcode {Retcode}, Message: {Message}",
+                json?.Retcode, json?.Message);
+            throw new CommandException("An error occurred while fetching Shiyu Defense data");
+        }
+
+        return json.Data!;
+    }
+}
---
+++ MehrakCore/Services/Commands/Zzz/Defense/ZzzDefenseCommandExecutor.cs
@@ -0,0 +1,28 @@
+﻿using MehrakCore.Repositories;
+using MehrakCore.Services.Commands.Executor;
+using MehrakCore.Services.Common;
+using Microsoft.Extensions.Logging;
+
+namespace MehrakCore.Services.Commands.Zzz.Defense;
+
+public class ZzzDefenseCommandExecutor : BaseCommandExecutor<ZzzDefenseCommandExecutor>
+{
+    public ZzzDefenseCommandExecutor(UserRepository userRepository,
+        TokenCacheService tokenCacheService,
+        IAuthenticationMiddlewareService authenticationMiddleware,
+        GameRecordApiService gameRecordApi,
+        ILogger<ZzzDefenseCommandExecutor> logger)
+        : base(userRepository, tokenCacheService, authenticationMiddleware, gameRecordApi, logger)
+    {
+    }
+
+    public override ValueTask ExecuteAsync(params object?[] parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task OnAuthenticationCompletedAsync(AuthenticationResult result)
+    {
+        throw new NotImplementedException();
+    }
+}
