ebd7bbe202035b23734a6d85721be6ffe9c7dfa3
GamerYuan <f2pplayer0801@gmail.com>
2025-09-09 18:38:51 +0800

    feat: add api response types

---
+++ MehrakCore/ApiResponseTypes/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MehrakCore.ApiResponseTypes;
+
+public class ApiResponse<T> where T : class
+{
+    public required T Data { get; init; }
+    public int Retcode { get; init; }
+    public required string Message { get; init; }
+}
