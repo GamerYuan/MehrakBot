ec982c5828629756a32ab5c15aae53c76b2bc3e7
GamerYuan <f2pplayer0801@gmail.com>
2025-10-11 19:04:32 +0800

    refactor: code cleanup

--- Mehrak.GameApi/Common/Types/BaseHoYoApiContext.cs
+++ Mehrak.GameApi/Common/Types/BaseHoYoApiContext.cs
@@ -4,12 +4,12 @@ namespace Mehrak.GameApi.Common.Types;
 
 public class BaseHoYoApiContext : IApiContext
 {
-    public ulong UserId { get; private init; }
+    public ulong UserId { get; }
 
-    public ulong LtUid { get; private init; }
-    public string LToken { get; private init; }
-    public string? GameUid { get; private init; }
-    public string? Region { get; private init; }
+    public ulong LtUid { get; }
+    public string LToken { get; }
+    public string? GameUid { get; }
+    public string? Region { get; }
 
     public BaseHoYoApiContext(ulong userId, ulong ltuid,
         string lToken, string? gameUid, string? region)
