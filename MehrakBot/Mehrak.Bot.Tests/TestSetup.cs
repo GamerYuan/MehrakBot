2400211c1f7d14284df8813abd795cf167912919
GamerYuan <f2pplayer0801@gmail.com>
2025-10-29 17:48:05 +0800

    test: bot layer test project

---
+++ MehrakBot/Mehrak.Bot.Tests/TestSetup.cs
@@ -0,0 +1,37 @@
+﻿#region
+
+using Mehrak.Infrastructure.Repositories;
+using Microsoft.Extensions.Logging.Abstractions;
+
+#endregion
+
+namespace Mehrak.Bot.Tests;
+
+[SetUpFixture]
+public class TestSetup
+{
+    private MongoTestHelper m_MongoTestHelper;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        m_MongoTestHelper = new MongoTestHelper();
+
+        var imageRepository =
+            new ImageRepository(m_MongoTestHelper.MongoDbService, NullLogger<ImageRepository>.Instance);
+
+        foreach (var image in Directory.EnumerateFiles(Path.Combine(AppContext.BaseDirectory, "Assets"),
+                     "*.png", SearchOption.AllDirectories))
+        {
+            var fileName = Path.GetFileNameWithoutExtension(image);
+            await using var stream = File.OpenRead(image);
+            await imageRepository.UploadFileAsync(fileName, stream);
+        }
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        m_MongoTestHelper.Dispose();
+    }
+}
