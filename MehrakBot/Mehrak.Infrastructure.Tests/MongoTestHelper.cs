8d35060176305c1dd5e27485d08881d9d359305d
GamerYuan <f2pplayer0801@gmail.com>
2025-10-30 16:48:19 +0800

    test: infrastructure test project

---
+++ MehrakBot/Mehrak.Infrastructure.Tests/MongoTestHelper.cs
@@ -0,0 +1,48 @@
+﻿#region
+
+#endregion
+
+namespace Mehrak.Infrastructure.Tests;
+
+public sealed class MongoTestHelper : IDisposable
+{
+    public static MongoTestHelper Instance { get; private set; } = null!;
+    public MongoDbService MongoDbService { get; }
+    public IImageRepository ImageRepository { get; }
+
+    private readonly MongoDbRunner m_MongoRunner;
+
+    private ulong m_TestUserId = 1_000_000_000;
+
+    public MongoTestHelper()
+    {
+        BsonSerializer.RegisterSerializer(new EnumSerializer<Game>(BsonType.String));
+        m_MongoRunner = MongoDbRunner.Start(logger: NullLogger<MongoDbRunner>.Instance);
+
+        Dictionary<string, string?> inMemorySettings = new()
+        {
+            ["MongoDB:ConnectionString"] = m_MongoRunner.ConnectionString,
+            ["MongoDB:DatabaseName"] = "TestDb"
+        };
+
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        MongoDbService = new MongoDbService(config, NullLogger<MongoDbService>.Instance);
+        ImageRepository = new ImageRepository(MongoDbService,
+            NullLogger<ImageRepository>.Instance);
+
+        Instance = this;
+    }
+
+    public ulong GetUniqueUserId()
+    {
+        return Interlocked.Increment(ref m_TestUserId);
+    }
+
+    public void Dispose()
+    {
+        m_MongoRunner.Dispose();
+    }
+}
---
+++ MehrakBot/Mehrak.Infrastructure.Tests/TestSetup.cs
@@ -0,0 +1,27 @@
+﻿namespace Mehrak.Infrastructure.Tests;
+
+[SetUpFixture]
+public class TestSetup
+{
+    private MongoTestHelper m_MongoTestHelper;
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        m_MongoTestHelper = new MongoTestHelper();
+
+        foreach (var image in Directory.EnumerateFiles(Path.Combine(AppContext.BaseDirectory, "Assets"),
+                     "*.png", SearchOption.AllDirectories))
+        {
+            var fileName = Path.GetFileNameWithoutExtension(image);
+            await using var stream = File.OpenRead(image);
+            await MongoTestHelper.Instance.ImageRepository.UploadFileAsync(fileName, stream);
+        }
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        m_MongoTestHelper.Dispose();
+    }
+}
