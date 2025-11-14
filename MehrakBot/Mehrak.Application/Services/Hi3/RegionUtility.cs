e7478f0b482ce1a4384edae71590fe09b9ec46d3
GamerYuan <f2pplayer0801@gmail.com>
2025-11-14 17:40:19 +0800

    feat: hi3 region utility

---
+++ MehrakBot/Mehrak.Application/Services/Hi3/RegionUtility.cs
@@ -0,0 +1,20 @@
+﻿using Mehrak.Domain.Enums;
+
+namespace Mehrak.Application.Services.Hi3;
+
+internal static class RegionUtility
+{
+    public static string ToRegion(this Hi3Server server)
+    {
+        return server switch
+        {
+            Hi3Server.SEA => "overseas01",
+            Hi3Server.JP => "jp01",
+            Hi3Server.KR => "kr01",
+            Hi3Server.America => "usa01",
+            Hi3Server.SAR => "asia01",
+            Hi3Server.Europe => "eur01",
+            _ => throw new ArgumentOutOfRangeException(nameof(server), server, null)
+        };
+    }
+}
