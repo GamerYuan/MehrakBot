89de4516d6c4189106a18ba5060455314b9f108e
GamerYuan <f2pplayer0801@gmail.com>
2025-11-20 10:16:17 +0800

    chore: hi3 app service extension

---
+++ MehrakBot/Mehrak.Application/Services/Hi3/Hi3ApplicationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Mehrak.Application.Services.Hi3.Character;
+using Mehrak.Application.Services.Hi3.Types;
+using Mehrak.Domain.Services.Abstractions;
+using Mehrak.GameApi.Hi3.Types;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mehrak.Application.Services.Hi3;
+
+internal static class Hi3ApplicationServiceExtensions
+{
+    public static IServiceCollection AddHi3ApplicationServices(this IServiceCollection services)
+    {
+        services.AddTransient<IApplicationService<Hi3CharacterApplicationContext>, Hi3CharacterApplicationService>();
+        services.AddSingleton<ICardService<Hi3CardGenerationContext<Hi3CharacterDetail>, Hi3CharacterDetail>, Hi3CharacterCardService>();
+
+        return services;
+    }
+}
