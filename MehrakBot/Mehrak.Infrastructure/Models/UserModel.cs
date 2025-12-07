4370cbeca919df1af571283063661e16a9991f15
GamerYuan <f2pplayer0801@gmail.com>
2025-12-07 16:56:57 +0800

    feat: sql user model

---
+++ MehrakBot/Mehrak.Infrastructure/Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using Mehrak.Domain.Enums;
+
+namespace Mehrak.Infrastructure.Models;
+
+internal class UserModel
+{
+    public long Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public List<UserProfileModel> Profiles { get; set; } = [];
+}
+
+internal class UserProfileModel
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+
+    public uint ProfileId { get; set; }
+    public long LtUid { get; set; }
+    public string LToken { get; set; } = string.Empty;
+    public DateTime? LastCheckIn { get; set; }
+}
+
+internal class GameUidEntry
+{
+    public long Id { get; set; }
+    public long ProfileId { get; set; }
+    public Game Game { get; set; }
+    public string Region { get; set; } = string.Empty;
+    public string GameUid { get; set; } = string.Empty;
+}
+
+internal class RegionEntry
+{
+    public long ProfileId { get; set; }
+    public Game Game { get; set; }
+    public string Region { get; set; } = string.Empty;
+}
