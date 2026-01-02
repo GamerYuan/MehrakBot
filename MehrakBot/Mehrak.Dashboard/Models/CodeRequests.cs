1b614d0f126d424207d5fe4c1d710855019ba866
GamerYuan <f2pplayer0801@gmail.com>
2026-01-02 11:34:59 +0800

    feat: code controller

---
+++ MehrakBot/Mehrak.Dashboard/Models/CodeRequests.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mehrak.Dashboard.Models;
+
+public abstract class CodeUpdateRequestBase
+{
+    [Required]
+    [StringLength(32, MinimumLength = 2)]
+    public string Game { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public List<string> Codes { get; set; } = [];
+}
+
+public sealed class AddCodesRequest : CodeUpdateRequestBase;
+
+public sealed class RemoveCodesRequest : CodeUpdateRequestBase;
