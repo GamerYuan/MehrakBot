1cc1d1fa17c3cfc8623de1f5431ac160186aec9d
GamerYuan <f2pplayer0801@gmail.com>
2026-01-10 16:36:50 +0800

    feat: add input validation to request

--- MehrakBot/Mehrak.Dashboard/Models/LoginRequest.cs
+++ MehrakBot/Mehrak.Dashboard/Models/LoginRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace Mehrak.Dashboard.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mehrak.Dashboard.Models;
 
 public class LoginRequest
 {
+    [Required, MinLength(1)]
     public string Username { get; set; } = default!;
+
+    [Required, MinLength(8)]
     public string Password { get; set; } = default!;
 }
