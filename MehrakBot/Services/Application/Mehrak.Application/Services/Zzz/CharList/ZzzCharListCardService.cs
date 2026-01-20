45d8c4c9db4d0abb3c1f203e0b321f2af0097129
GamerYuan <f2pplayer0801@gmail.com>
2026-01-20 15:31:35 +0800

    feat: update zzz charlist card layout

--- MehrakBot/Services/Application/Mehrak.Application/Services/Zzz/CharList/ZzzCharListCardService.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Services/Zzz/CharList/ZzzCharListCardService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Numerics;
+using System.Text.Json;
 using Mehrak.Application.Models;
 using Mehrak.Application.Services.Abstractions;
 using Mehrak.Application.Utility;
@@ -9,8 +10,10 @@ using Mehrak.Domain.Services.Abstractions;
 using Mehrak.GameApi.Zzz.Types;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace Mehrak.Application.Services.Zzz.CharList;
 
@@ -49,14 +52,20 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
 
     public async Task InitializeAsync(CancellationToken cancellationToken = default)
     {
-        List<Task<(int x, Image)>> weaponStarTasks =
+        List<Task<(int x, Image)>> starTasks =
         [
             .. Enumerable.Range(1, 5)
                 .Select(async i => (i, await Image.LoadAsync(
                     await m_ImageRepository.DownloadFileToStreamAsync($"zzz_weapon_star_{i}"))))
         ];
 
-        m_StarImages = (await Task.WhenAll(weaponStarTasks)).ToDictionary();
+        var starImage = await Task.WhenAll(starTasks);
+        foreach (var item in starImage)
+        {
+            item.Item2.Mutate(x => x.Resize(0, 30));
+        }
+
+        m_StarImages = starImage.ToDictionary();
 
         m_Logger.LogInformation(LogMessage.ServiceInitialized, nameof(ZzzCharListCardService));
     }
@@ -76,31 +85,68 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
                 context.GameProfile.GameUid, charData.Count);
 
             var avatarImages = await charData
+                .OrderByDescending(x => x.Level)
+                .ThenByDescending(x => x.Rarity)
+                .ThenBy(x => x.Name)
                 .ToAsyncEnumerable()
                 .Select(async (x, token) =>
                 {
                     var image = await Image.LoadAsync(await m_ImageRepository.DownloadFileToStreamAsync(x.ToImageName(), token), token);
-                    ZzzAvatar avatar = new(x.Id, x.Level, x.Rarity[0], x.Rank, image);
-                    return (Avatar: avatar, Image: avatar.GetStyledAvatarImage());
-                })
-                .ToDictionaryAsync(x => x.Avatar, x => x.Image, ZzzAvatarIdComparer.Instance);
-            disposables.AddRange(avatarImages.Keys);
-            disposables.AddRange(avatarImages.Values);
+                    using ZzzAvatar avatar = new(x.Id, x.Level, x.Rarity[0], x.Rank, image);
+                    return avatar.GetStyledAvatarImage();
+                }).ToListAsync();
+            disposables.AddRange(avatarImages);
 
             var buddyImages = await buddyData
+                .OrderByDescending(x => x.Level)
+                .ThenByDescending(x => x.Star)
+                .ThenBy(x => x.Name)
                 .ToAsyncEnumerable()
                 .Select(async (x, token) =>
                 {
                     using var image = await Image.LoadAsync(await m_ImageRepository.DownloadFileToStreamAsync(x!.ToImageName(), token), token);
-                    return (BuddyId: x!.Id, Image: x.GetStyledBuddyImage(image));
+                    return x.GetStyledBuddyImage(image, m_StarImages[x.Star]);
                 })
-                .ToDictionaryAsync(x => x.BuddyId, x => x.Image);
-            disposables.AddRange(buddyImages.Values);
+                .ToListAsync();
+            disposables.AddRange(buddyImages);
 
             var layout = ImageUtility.CalculateSplitGridLayout(avatarImages.Count, buddyImages.Count,
                 150, 180, [120, 50, 50, 50], 20, 40);
 
             using Image<Rgba32> background = new(layout.OutputWidth, layout.OutputHeight + 50);
+            background.Mutate(ctx =>
+            {
+                ctx.Clear(Color.FromRgb(69, 69, 69));
+
+                ctx.DrawText(new RichTextOptions(m_TitleFont)
+                {
+                    Origin = new Vector2(50, 80),
+                    VerticalAlignment = VerticalAlignment.Bottom
+                }, $"{context.GameProfile.Nickname}·AR {context.GameProfile.Level}", Color.White);
+
+                ctx.DrawText(new RichTextOptions(m_NormalFont)
+                {
+                    Origin = new Vector2(50, 110),
+                    VerticalAlignment = VerticalAlignment.Bottom
+                }, context.GameProfile.GameUid!, Color.White);
+
+                var i = 0;
+
+                while (i < avatarImages.Count)
+                {
+                    var pos = layout.ImagePositions[i];
+                    ctx.DrawImage(avatarImages[i], new Point(pos.X, pos.Y), 1f);
+                    i++;
+                }
+
+                i = 0;
+                while (i < buddyImages.Count)
+                {
+                    var pos = layout.ImagePositions[avatarImages.Count + i];
+                    ctx.DrawImage(buddyImages[i], new Point(pos.X, pos.Y), 1f);
+                    i++;
+                }
+            });
 
             MemoryStream stream = new();
             await background.SaveAsJpegAsync(stream, JpegEncoder);
