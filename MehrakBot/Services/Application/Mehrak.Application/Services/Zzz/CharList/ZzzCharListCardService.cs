4cab0fd3019263953683a98161902629007d330c
GamerYuan <f2pplayer0801@gmail.com>
2026-01-20 16:30:15 +0800

    feat: update char card layout

--- MehrakBot/Services/Application/Mehrak.Application/Services/Zzz/CharList/ZzzCharListCardService.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Services/Zzz/CharList/ZzzCharListCardService.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Globalization;
+using System.Numerics;
 using System.Text.Json;
 using Mehrak.Application.Models;
 using Mehrak.Application.Services.Abstractions;
@@ -10,6 +11,7 @@ using Mehrak.Domain.Services.Abstractions;
 using Mehrak.GameApi.Zzz.Types;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
@@ -35,6 +37,33 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
     private readonly Font m_SmallFont;
 
     private Dictionary<int, Image> m_StarImages = [];
+
+    private static readonly Dictionary<string, Color> ElementForeground = new(StringComparer.OrdinalIgnoreCase)
+    {
+        { "Physical", Color.FromRgb(244, 163, 111) },
+        { "Fire", Color.FromRgb(7, 229, 252) },
+        { "Ice", Color.FromRgb(203, 253, 253) },
+        { "Electric", Color.FromRgb(222, 186, 255) },
+        { "Ether", Color.FromRgb(163, 238, 202) },
+    };
+
+    private static readonly Dictionary<string, Color> ElementBackground = new(StringComparer.OrdinalIgnoreCase)
+    {
+        { "Physical", Color.FromRgba(198, 90, 21, 128) },
+        { "Fire", Color.FromRgba(25, 156, 198, 128) },
+        { "Ice", Color.FromRgba(108, 192, 192, 128) },
+        { "Electric", Color.FromRgba(177, 117, 217, 128) },
+        { "Ether", Color.FromRgba(56, 185, 145, 128) },
+    };
+
+    private static readonly char[] RarityOrder = ['S', 'A'];
+
+    private static readonly Color GoldBackgroundColor = Color.FromRgb(183, 125, 76);
+    private static readonly Color PurpleBackgroundColor = Color.FromRgb(132, 104, 173);
+
+    private static readonly Color PurpleForegroundColor = Color.FromRgb(204, 173, 255);
+
+    private static readonly TextInfo TextInfo = new CultureInfo("en-US", false).TextInfo;
 
     public ZzzCharListCardService(IImageRepository imageRepository, IApplicationMetrics metrics, ILogger<ZzzCharListCardService> logger)
     {
@@ -111,9 +140,27 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
             disposables.AddRange(buddyImages);
 
             var layout = ImageUtility.CalculateSplitGridLayout(avatarImages.Count, buddyImages.Count,
-                150, 180, [120, 50, 50, 50], 20, 40);
+                150, 180, [120, 50, 50, 50], 20, 80);
 
-            using Image<Rgba32> background = new(layout.OutputWidth, layout.OutputHeight + 50);
+            var charCountByElem = charData
+                .GroupBy(x => x.ElementType)
+                .OrderBy(x => x.Key)
+                .Select(x => new { Element = ToTitleCase(StatUtils.GetElementNameFromId(x.Key, 0)), Count = x.Count() })
+                .ToList();
+
+            var charCountByProfession = charData
+                .GroupBy(x => x.AvatarProfession)
+                .OrderBy(x => x.Key)
+                .Select(x => new { Profession = StatUtils.GetProfessionNameFromId(x.Key), Count = x.Count() })
+                .ToList();
+
+            var charCountByRarity = charData
+                .GroupBy(x => x.Rarity.ToUpperInvariant())
+                .OrderBy(x => RarityOrder.IndexOf(x.Key[0]))
+                .Select(x => new { Rarity = $"{x.Key}-Rank", Count = x.Count() })
+                .ToList();
+
+            using Image<Rgba32> background = new(layout.OutputWidth, layout.OutputHeight + 180);
             background.Mutate(ctx =>
             {
                 ctx.Clear(Color.FromRgb(69, 69, 69));
@@ -122,7 +169,7 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
                 {
                     Origin = new Vector2(50, 80),
                     VerticalAlignment = VerticalAlignment.Bottom
-                }, $"{context.GameProfile.Nickname}·AR {context.GameProfile.Level}", Color.White);
+                }, $"{context.GameProfile.Nickname}·IK {context.GameProfile.Level}", Color.White);
 
                 ctx.DrawText(new RichTextOptions(m_NormalFont)
                 {
@@ -146,6 +193,100 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
                     ctx.DrawImage(buddyImages[i], new Point(pos.X, pos.Y), 1f);
                     i++;
                 }
+
+                var yOffset = layout.OutputHeight - 25;
+                var xOffset = 50;
+
+                foreach (var entry in charCountByRarity)
+                {
+                    var countSize = TextMeasurer.MeasureSize(entry.Count.ToString(),
+                        new TextOptions(m_NormalFont));
+                    var elemSize =
+                        TextMeasurer.MeasureSize(entry.Rarity, new TextOptions(m_NormalFont));
+                    FontRectangle size = new(0, 0, countSize.Width + elemSize.Width + 20,
+                        countSize.Height + elemSize.Height);
+                    var overlay =
+                        ImageUtility.CreateRoundedRectanglePath((int)size.Width + 50, 50, 10)
+                            .Translate(xOffset, yOffset);
+                    EllipsePolygon foreground = new(new PointF(xOffset + 20, yOffset + 25), 10);
+                    ctx.Fill(entry.Rarity == "S-Rank" ? GoldBackgroundColor.WithAlpha(128) : PurpleBackgroundColor.WithAlpha(128), overlay);
+                    ctx.Fill(entry.Rarity == "S-Rank" ? Color.Gold : PurpleForegroundColor, foreground);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 40, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Rarity, Color.White);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 35 + size.Width, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Count.ToString(), Color.White);
+                    xOffset += (int)size.Width + 70;
+                }
+
+                yOffset += 60;
+                xOffset = 50;
+
+                foreach (var entry in charCountByElem)
+                {
+                    var countSize = TextMeasurer.MeasureSize(entry.Count.ToString(),
+                        new TextOptions(m_NormalFont));
+                    var elemSize = TextMeasurer.MeasureSize(entry.Element, new TextOptions(m_NormalFont));
+                    FontRectangle size = new(0, 0, countSize.Width + elemSize.Width + 20,
+                        countSize.Height + elemSize.Height);
+                    var overlay =
+                        ImageUtility.CreateRoundedRectanglePath((int)size.Width + 50, 50, 10)
+                            .Translate(xOffset, yOffset);
+                    EllipsePolygon foreground = new(new PointF(xOffset + 20, yOffset + 25), 10);
+                    ctx.Fill(ElementBackground[entry.Element], overlay);
+                    ctx.Fill(ElementForeground[entry.Element], foreground);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 40, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Element, Color.White);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 35 + size.Width, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Count.ToString(), Color.White);
+                    xOffset += (int)size.Width + 70;
+                }
+
+                yOffset += 60;
+                xOffset = 50;
+
+                foreach (var entry in charCountByProfession)
+                {
+                    var countSize = TextMeasurer.MeasureSize(entry.Count.ToString(),
+                        new TextOptions(m_NormalFont));
+                    var elemSize = TextMeasurer.MeasureSize(entry.Profession, new TextOptions(m_NormalFont));
+                    FontRectangle size = new(0, 0, countSize.Width + elemSize.Width + 20,
+                        countSize.Height + elemSize.Height);
+                    var overlay =
+                        ImageUtility.CreateRoundedRectanglePath((int)size.Width + 50, 50, 10)
+                            .Translate(xOffset, yOffset);
+                    EllipsePolygon foreground = new(new PointF(xOffset + 20, yOffset + 25), 10);
+                    ctx.Fill(Color.FromRgb(24, 24, 24), overlay);
+                    ctx.Fill(Color.White, foreground);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 40, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Profession, Color.White);
+                    ctx.DrawText(new RichTextOptions(m_NormalFont)
+                    {
+                        Origin = new Vector2(xOffset + 35 + size.Width, yOffset + 32),
+                        HorizontalAlignment = HorizontalAlignment.Right,
+                        VerticalAlignment = VerticalAlignment.Center
+                    }, entry.Count.ToString(), Color.White);
+                    xOffset += (int)size.Width + 70;
+                }
             });
 
             MemoryStream stream = new();
@@ -163,5 +304,10 @@ public class ZzzCharListCardService : ICardService<(IEnumerable<ZzzBasicAvatarDa
         {
             foreach (var disposable in disposables) disposable.Dispose();
         }
+    }
+
+    private static string ToTitleCase(string str)
+    {
+        return TextInfo.ToTitleCase(str.ToLowerInvariant());
     }
 }
