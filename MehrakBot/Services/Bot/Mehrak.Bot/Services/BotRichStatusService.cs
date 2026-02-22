6799ec7e51e7446a6afd45681c74401816dd8450
GamerYuan <f2pplayer0801@gmail.com>
2026-02-22 16:11:58 +0800

    feat: bot rich status update

---
+++ MehrakBot/Services/Bot/Mehrak.Bot/Services/BotRichStatusService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetCord.Gateway;
+
+namespace Mehrak.Bot.Services;
+
+internal class BotRichStatusService : BackgroundService
+{
+    private const int RefreshMinutes = 60;
+
+    private readonly GatewayClient m_Client;
+    private readonly ClickhouseClientService m_ClickhouseClient;
+    private readonly ILogger<BotRichStatusService> m_Logger;
+
+    public BotRichStatusService(GatewayClient gatewayClient, ClickhouseClientService clickhouseClient, ILogger<BotRichStatusService> logger)
+    {
+        m_Client = gatewayClient;
+        m_ClickhouseClient = clickhouseClient;
+        m_Logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _ = UpdateStatusTask(stoppingToken);
+    }
+
+    private async Task UpdateStatusTask(CancellationToken token)
+    {
+        while (m_Client.Status != WebSocketStatus.Ready)
+        {
+            await Task.Delay(50, token);
+        }
+
+        while (!token.IsCancellationRequested)
+        {
+            var userCount = await m_ClickhouseClient.GetUniqueUserCountAsync();
+            m_Logger.LogDebug("Updating presence with {UserCount} users", userCount);
+            if (userCount > 0)
+            {
+                await m_Client.UpdatePresenceAsync(new PresenceProperties(NetCord.UserStatusType.Online)
+                {
+                    Activities =
+                    [
+                        new UserActivityProperties($"with {userCount} users", UserActivityType.Playing)
+                    ]
+                }, cancellationToken: token);
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(RefreshMinutes), token);
+        }
+    }
+}
