80010b93e3ecb6834b3a2304024fc7043880bd89
GamerYuan <f2pplayer0801@gmail.com>
2026-02-26 00:02:14 +0800

    chore: add ef migrations

--- MehrakBot/Common/Mehrak.Infrastructure/Migrations/20260225151926_Indexing.Designer.cs
+++ MehrakBot/Common/Mehrak.Infrastructure/Migrations/20260225151926_Indexing.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Mehrak.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Mehrak.Infrastructure.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225151926_Indexing")]
+    partial class Indexing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -48,7 +51,7 @@ namespace Mehrak.Infrastructure.Migrations
 
                     b.HasIndex("ProfileId");
 
-                    b.ToTable("GameUids", (string)null);
+                    b.ToTable("GameUids");
                 });
 
             modelBuilder.Entity("Mehrak.Infrastructure.Models.ProfileRegion", b =>
@@ -73,7 +76,7 @@ namespace Mehrak.Infrastructure.Migrations
 
                     b.HasIndex("ProfileId");
 
-                    b.ToTable("Regions", (string)null);
+                    b.ToTable("Regions");
                 });
 
             modelBuilder.Entity("Mehrak.Infrastructure.Models.UserModel", b =>
@@ -92,7 +95,7 @@ namespace Mehrak.Infrastructure.Migrations
                     b.HasIndex("Id")
                         .IsUnique();
 
-                    b.ToTable("Users", (string)null);
+                    b.ToTable("Users");
                 });
 
             modelBuilder.Entity("Mehrak.Infrastructure.Models.UserProfileModel", b =>
@@ -126,7 +129,7 @@ namespace Mehrak.Infrastructure.Migrations
                     b.HasIndex("UserId", "ProfileId")
                         .IsUnique();
 
-                    b.ToTable("UserProfiles", (string)null);
+                    b.ToTable("UserProfiles");
                 });
 
             modelBuilder.Entity("Mehrak.Infrastructure.Models.ProfileGameUid", b =>
---
+++ MehrakBot/Common/Mehrak.Infrastructure/Migrations/20260225151926_Indexing.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mehrak.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Indexing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id",
+                table: "Users",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_LtUid",
+                table: "UserProfiles",
+                column: "LtUid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId_ProfileId",
+                table: "UserProfiles",
+                columns: new[] { "UserId", "ProfileId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Id",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_LtUid",
+                table: "UserProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_UserId_ProfileId",
+                table: "UserProfiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles",
+                column: "UserId");
+        }
+    }
+}
