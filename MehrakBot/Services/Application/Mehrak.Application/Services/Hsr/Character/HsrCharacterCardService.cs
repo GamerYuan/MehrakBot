487764be2266fcb76037a39d7c27bbb1f2f438ef
GamerYuan <f2pplayer0801@gmail.com>
2026-02-25 19:32:03 +0800

    feat: add elation stat

--- MehrakBot/Services/Application/Mehrak.Application/Services/Hsr/Character/HsrCharacterCardService.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Services/Hsr/Character/HsrCharacterCardService.cs
@@ -78,13 +78,7 @@ public class HsrCharacterCardService : ICardService<HsrCharacterInformation>, IA
 
     public async Task InitializeAsync(CancellationToken cancellationToken = default)
     {
-        int[] statIds =
-        [
-            1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60
-        ];
-
-        m_StatImages = await statIds.ToAsyncEnumerable().Where(x => x != 8).Select(async (x, token) =>
+        m_StatImages = await StatMappingUtility.HsrMapping.Keys.ToAsyncEnumerable().Where(x => x != 8).Select(async (x, token) =>
         {
             var path = string.Format(StatsPath, x);
             var image = await Image.LoadAsync(await m_ImageRepository.DownloadFileToStreamAsync(path), token);
