171efe1a9eaf9d056e5861e6f4ac3f12573b8ff3
GamerYuan <f2pplayer0801@gmail.com>
2026-02-26 07:49:17 +0800

    fix: initialise character cache

--- MehrakBot/Services/Application/Mehrak.Application/ApplicationServiceCollectionExtension.cs
+++ MehrakBot/Services/Application/Mehrak.Application/ApplicationServiceCollectionExtension.cs
@@ -7,6 +7,7 @@ using Mehrak.Application.Services.Hi3;
 using Mehrak.Application.Services.Hsr;
 using Mehrak.Application.Services.Zzz;
 using Mehrak.Domain.Common;
+using Mehrak.Infrastructure.Services;
 
 #endregion
 
@@ -22,6 +23,7 @@ internal static class ApplicationServiceCollectionExtension
         services.AddKeyedTransient<IApplicationService, CodeRedeemApplicationService>(CommandName.Zzz.Codes);
 
         services.AddHostedService<AsyncInitializationHostedService>();
+        services.AddHostedService<CharacterCacheBackgroundService>();
 
         services.AddGenshinApplicationServices();
         services.AddHsrApplicationServices();
