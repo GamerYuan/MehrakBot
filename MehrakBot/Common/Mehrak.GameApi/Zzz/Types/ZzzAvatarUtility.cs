3ac897e3c740a2e1494d56b8c8c7e0c0d941e10c
GamerYuan <f2pplayer0801@gmail.com>
2026-03-05 09:43:02 +0800

    feat: add url parsing guard clause

--- MehrakBot/Common/Mehrak.GameApi/Zzz/Types/ZzzAvatarUtility.cs
+++ MehrakBot/Common/Mehrak.GameApi/Zzz/Types/ZzzAvatarUtility.cs
@@ -7,7 +7,12 @@ internal static class ZzzAvatarUtility
 {
     internal static string GetAvatarImageName(int avatarId, string avatarUrl)
     {
-        var uri = new Uri(avatarUrl);
+        if (string.IsNullOrWhiteSpace(avatarUrl) ||
+            !Uri.TryCreate(avatarUrl, UriKind.Absolute, out var uri))
+        {
+            return string.Format(FileNameFormat.Zzz.AvatarName, avatarId);
+        }
+
         var hasSkin = Regex.Match(Path.GetFileNameWithoutExtension(uri.LocalPath), $@".*_({avatarId}_\d+)$");
         if (hasSkin.Success)
             return string.Format(FileNameFormat.Zzz.AvatarName, hasSkin.Groups[1].Value);
