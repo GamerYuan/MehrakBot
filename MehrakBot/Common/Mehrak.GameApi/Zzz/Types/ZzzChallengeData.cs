6622e76f6346480f48b6fd656d2c537eb4c25e8c
GamerYuan <f2pplayer0801@gmail.com>
2026-03-30 07:42:44 +0800

    chore: code cleanup

--- MehrakBot/Common/Mehrak.GameApi/Zzz/Types/ZzzChallengeData.cs
+++ MehrakBot/Common/Mehrak.GameApi/Zzz/Types/ZzzChallengeData.cs
@@ -63,17 +63,6 @@ public class ZzzDefenseDataWrapper
     [JsonPropertyName("icon")] public required string Icon { get; init; }
 }
 
-[Obsolete("This class is deprecated. Use ZzzDefenseDataV2 instead.")]
-public class ZzzDefenseData
-{
-    [JsonPropertyName("begin_time")] public required string BeginTime { get; init; }
-    [JsonPropertyName("end_time")] public required string EndTime { get; init; }
-    [JsonPropertyName("rating_list")] public required List<RatingData> RatingList { get; init; }
-    [JsonPropertyName("has_data")] public bool HasData { get; init; }
-    [JsonPropertyName("all_floor_detail")] public required List<FloorDetail> AllFloorDetail { get; init; }
-}
-
-
 public class ZzzDefenseDataV2
 {
     [JsonPropertyName("zone_id")] public int ZoneId { get; init; }
@@ -139,24 +128,6 @@ public class RatingData
 {
     [JsonPropertyName("times")] public int Times { get; init; }
     [JsonPropertyName("rating")] public required string Rating { get; init; }
-}
-
-[Obsolete]
-public class FloorDetail
-{
-    [JsonPropertyName("layer_index")] public int LayerIndex { get; init; }
-    [JsonPropertyName("rating")] public required string Rating { get; init; }
-    [JsonPropertyName("node_1")] public required NodeData Node1 { get; init; }
-    [JsonPropertyName("node_2")] public required NodeData Node2 { get; init; }
-    [JsonPropertyName("zone_name")] public required string ZoneName { get; init; }
-}
-
-[Obsolete]
-public class NodeData
-{
-    [JsonPropertyName("avatars")] public required List<ZzzChallengeAvatar> Avatars { get; init; }
-    [JsonPropertyName("buddy")] public required ZzzBuddy? Buddy { get; init; }
-    [JsonPropertyName("battle_time")] public int BattleTime { get; init; }
 }
 
 public class AssaultFloorDetail
