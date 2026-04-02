9522e377ed0330f1f4e5ff7be4c6cee91566a64c
GamerYuan <f2pplayer0801@gmail.com>
2026-04-02 13:50:54 +0800

    feat: hyl post api service and type

---
+++ MehrakBot/Common/Mehrak.GameApi/Common/Types/HylPost.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mehrak.GameApi.Common.Types;
+
+public class HylPostWrapper
+{
+    [JsonPropertyName("post")] public required HylPost Post { get; set; }
+}
+
+public class HylPost
+{
+    [JsonPropertyName("post")] public required HylPostDetail Post { get; set; }
+}
+
+public class HylPostDetail
+{
+    [JsonPropertyName("post_id")] public required string PostId { get; set; }
+    [JsonPropertyName("uid")] public required string Uid { get; set; }
+    [JsonPropertyName("title")] public required string Title { get; set; }
+    [JsonPropertyName("created_at")] public long CreatedAt { get; set; }
+    [JsonPropertyName("image_list")] public required List<HylImage> ImageList { get; set; }
+    [JsonPropertyName("cover_list")] public required List<HylImage> CoverList { get; set; }
+    [JsonPropertyName("structured_content")] public required string StructuredContent { get; set; }
+}
+
+public class HylImage
+{
+    [JsonPropertyName("url")] public required string Url { get; set; }
+    [JsonPropertyName("height")] public int Height { get; set; }
+    [JsonPropertyName("width")] public int Width { get; set; }
+    [JsonPropertyName("format")] public required string Format { get; set; }
+    [JsonPropertyName("size")] public required string Size { get; set; }
+    [JsonPropertyName("spoiler")] public bool Spoiler { get; set; }
+}
---
+++ MehrakBot/Common/Mehrak.GameApi/Common/Types/HylPostApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Mehrak.Domain.Models.Abstractions;
+
+namespace Mehrak.GameApi.Common.Types;
+
+public class HylPostApiContext : IApiContext
+{
+    public ulong UserId { get; }
+    public long PostId { get; }
+    public WikiLocales Locale { get; }
+
+    public HylPostApiContext(ulong userId, long postId, WikiLocales locale = WikiLocales.EN)
+    {
+        UserId = userId;
+        PostId = postId;
+        Locale = locale;
+    }
+}
