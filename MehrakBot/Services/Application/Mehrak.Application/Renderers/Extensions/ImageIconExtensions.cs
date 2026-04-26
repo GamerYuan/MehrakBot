095705466aa2d18c421aeac908ddf6824faa65a8
GamerYuan <f2pplayer0801@gmail.com>
2026-04-26 15:23:56 +0800

    feat: update centered icon layout with padding

--- MehrakBot/Services/Application/Mehrak.Application/Renderers/Extensions/ImageIconExtensions.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Renderers/Extensions/ImageIconExtensions.cs
@@ -20,7 +20,7 @@ public static class ImageIconExtensions
         if (radius <= 0)
             throw new ArgumentOutOfRangeException(nameof(radius), "Radius must be positive.");
 
-        var iconSize = radius * 2 - padding;
+        var iconSize = (radius - padding) * 2;
 
         if (iconSize <= 0)
             throw new ArgumentOutOfRangeException(nameof(padding), "Padding must be smaller than the icon diameter.");
