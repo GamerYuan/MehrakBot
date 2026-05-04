3b6e25d441e3bb9ce9927e436b2c2c8b86d1dadf
GamerYuan <f2pplayer0801@gmail.com>
2026-05-04 21:55:37 +0800

    feat: character portrait db model and migrations

---
+++ MehrakBot/Common/Mehrak.Infrastructure/Migrations/CharacterDb/20260504135428_PortraitPosition.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mehrak.Infrastructure.Migrations.CharacterDb
+{
+    /// <inheritdoc />
+    public partial class PortraitPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
