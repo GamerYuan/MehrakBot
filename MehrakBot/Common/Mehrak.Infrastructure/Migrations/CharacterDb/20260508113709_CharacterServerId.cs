b5129406efd751dd1479f301c928d0e46c864552
GamerYuan <f2pplayer0801@gmail.com>
2026-05-08 19:37:20 +0800

    chore: add ef migrations

---
+++ MehrakBot/Common/Mehrak.Infrastructure/Migrations/CharacterDb/20260508113709_CharacterServerId.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Mehrak.Infrastructure.Migrations.CharacterDb
+{
+    /// <inheritdoc />
+    public partial class CharacterServerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CharacterServerIds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CharacterId = table.Column<int>(type: "integer", nullable: false),
+                    ServerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterServerIds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CharacterServerIds_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterServerIds_CharacterId",
+                table: "CharacterServerIds",
+                column: "CharacterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterServerIds");
+        }
+    }
+}
