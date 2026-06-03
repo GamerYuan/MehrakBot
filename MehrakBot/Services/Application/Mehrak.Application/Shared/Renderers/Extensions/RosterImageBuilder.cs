1d0767f10ca6074b7b3bc28f82abf70c1ad9fb44
GamerYuan <f2pplayer0801@gmail.com>
2026-06-03 14:31:52 +0800

    refactor: cleanup obsolete methods

--- MehrakBot/Services/Application/Mehrak.Application/Shared/Renderers/Extensions/RosterImageBuilder.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Shared/Renderers/Extensions/RosterImageBuilder.cs
@@ -17,43 +17,6 @@ public record RosterLayout(
 
 public static class RosterImageBuilder
 {
-    [Obsolete]
-    public static Image<Rgba32> Build(
-        IEnumerable<Image> avatars,
-        RosterLayout layout,
-        Image? trailingImage = null)
-    {
-        var avatarList = avatars as IReadOnlyList<Image> ?? [.. avatars];
-        var totalSlots = avatarList.Count + (trailingImage != null ? 1 : 0);
-        var canvasWidth = layout.MaxSlots * layout.AvatarWidth + (layout.MaxSlots - 1) * layout.Spacing + 20;
-
-        var offset = (layout.MaxSlots - totalSlots) * layout.AvatarWidth / 2 + 10;
-
-        Image<Rgba32> rosterImage = new(canvasWidth, layout.CanvasHeight, Color.Transparent.ToPixel<Rgba32>());
-
-        rosterImage.Mutate(ctx =>
-        {
-            ctx.Paint(canvas =>
-            {
-                for (var i = 0; i < avatarList.Count; i++)
-                {
-                    var x = offset + i * (layout.AvatarWidth + layout.Spacing);
-                    canvas.DrawImage(avatarList[i], avatarList[i].Bounds,
-                        new RectangleF(x, 0, avatarList[i].Width, avatarList[i].Height), KnownResamplers.Bicubic);
-                }
-
-                if (trailingImage != null)
-                {
-                    var x = offset + avatarList.Count * (layout.AvatarWidth + layout.Spacing);
-                    canvas.DrawImage(trailingImage, trailingImage.Bounds,
-                        new RectangleF(x, 0, trailingImage.Width, trailingImage.Height), KnownResamplers.Bicubic);
-                }
-            });
-        });
-
-        return rosterImage;
-    }
-
     public static void Draw<TItem>(
         IEnumerable<TItem> items,
         RosterLayout layout,
--- MehrakBot/Services/Application/Mehrak.Application/Zzz/AvatarImageUtility.cs
+++ MehrakBot/Services/Application/Mehrak.Application/Zzz/AvatarImageUtility.cs
@@ -66,57 +66,6 @@ internal static class AvatarImageUtility
         }
     }
 
-    [Obsolete]
-    public static Image<Rgba32> GetStyledAvatarImage(this ZzzAvatar avatar, string text = "")
-    {
-        Image<Rgba32> avatarImage = new(150, 180);
-        RectangleF rectangle = new(0, 145, 150, 35);
-
-        avatarImage.Mutate(ctx =>
-        {
-            ctx.Paint(canvas =>
-            {
-                canvas.Fill(Brushes.Solid(avatar.Rarity == 'A' ? PurpleBackgroundColor : GoldBackgroundColor), new Rectangle(0, 0, avatarImage.Width, avatarImage.Height));
-            });
-
-            ctx.Paint(canvas =>
-            {
-                canvas.DrawImage(avatar.AvatarImage, avatar.AvatarImage.Bounds,
-                    new RectangleF(0, 0, avatar.AvatarImage.Width, avatar.AvatarImage.Height), KnownResamplers.Bicubic);
-                canvas.Fill(Brushes.Solid(Color.Black), new Rectangle((int)rectangle.X, (int)rectangle.Y, (int)rectangle.Width, (int)rectangle.Height));
-                canvas.DrawText(new RichTextOptions(NormalFont)
-                {
-                    Origin = new PointF(75, 160),
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center
-                }, string.IsNullOrEmpty(text) ? $"Lv. {avatar.Level}" : text, Brushes.Solid(Color.White), null);
-                if (avatar.Rank > 0)
-                {
-                    canvas.DrawRoundedRectangleOverlay(30, 30, new PointF(115, 110),
-                        new RoundedRectangleOverlayStyle(
-                            avatar.Rank == 6 ? Color.Gold : NormalConstColor,
-                            CornerRadius: 5));
-                    canvas.Restore(); // Restore before drawing text due to bug in ImageSharp.Drawing 3.0.0
-                    canvas.DrawText(new RichTextOptions(NormalFont)
-                    {
-                        Origin = new PointF(130, 125),
-                        HorizontalAlignment = HorizontalAlignment.Center,
-                        VerticalAlignment = VerticalAlignment.Center
-                    },
-                        avatar.Rank.ToString(),
-                        Brushes.Solid(avatar.Rank == 6 ? GoldConstTextColor : Color.White), null);
-                }
-
-                var border = ImageUtility.CreateRoundedRectanglePath(150, 180, 15);
-                canvas.Draw(Pens.Solid(Color.Black, 4), border);
-            });
-
-            ctx.ApplyRoundedCorners(15);
-        });
-
-        return avatarImage;
-    }
-
     public static void DrawStyledBuddyImage(this ZzzBuddyData buddy, DrawingCanvas canvas, Point location, Image buddyImage, Image starImage)
     {
         using var region = canvas.CreateRegion(new Rectangle(location, new Size(150, 180)));
@@ -140,43 +89,5 @@ internal static class AvatarImageUtility
 
         region.DrawImage(starImage, starImage.Bounds,
             new RectangleF(65, 120, starImage.Width, starImage.Height), KnownResamplers.Bicubic);
-    }
-
-    [Obsolete]
-    public static Image<Rgba32> GetStyledBuddyImage(this ZzzBuddyData buddy, Image buddyImage, Image starImage)
-    {
-        Image<Rgba32> background = new(150, 180);
-        RectangleF rectangle = new(0, 145, 150, 35);
-
-        background.Mutate(ctx =>
-        {
-            ctx.Paint(canvas =>
-            {
-                canvas.Fill(Brushes.Solid(Color.FromPixel(new Rgb24(24, 24, 24))), new Rectangle(0, 0, background.Width, background.Height));
-            });
-
-            ctx.Paint(canvas =>
-            {
-                var border = ImageUtility.CreateRoundedRectanglePath(150, 180, 15);
-
-                canvas.DrawImage(buddyImage, buddyImage.Bounds,
-                    new RectangleF(-45, -20, buddyImage.Width, buddyImage.Height), KnownResamplers.Bicubic);
-
-                canvas.Fill(Brushes.Solid(Color.Black), new Rectangle((int)rectangle.X, (int)rectangle.Y, (int)rectangle.Width, (int)rectangle.Height));
-                canvas.DrawText(new RichTextOptions(NormalFont)
-                {
-                    Origin = new PointF(75, 160),
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center
-                }, $"Lv. {buddy.Level}", Brushes.Solid(Color.White), null);
-
-                canvas.Draw(Pens.Solid(Color.Black, 4), border);
-                canvas.DrawImage(starImage, starImage.Bounds,
-                    new RectangleF(65, 120, starImage.Width, starImage.Height), KnownResamplers.Bicubic);
-            });
-
-            ctx.ApplyRoundedCorners(15);
-        });
-        return background;
     }
 }
