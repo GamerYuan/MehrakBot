aa01fa709af4bd5aec5c984b9b2e7c52bc4d4e20
GamerYuan <f2pplayer0801@gmail.com>
2026-09-07 13:32:40 +0800

    fix(apphost): wait for completed database migrations

---
+++ MehrakBot/Mehrak.AppHost.Tests/MigrationDependencyTests.cs
@@ -0,0 +1,33 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace Mehrak.AppHost.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
+internal sealed class MigrationDependencyTests
+{
+    [Test]
+    public void RuntimeServices_WaitForSuccessfulMigrationCompletion()
+    {
+        var builder = AppHostProgram.CreateBuilder([]);
+
+        var migrationService = GetResource(builder, "migration-service");
+        foreach (var resourceName in new[] { "application", "bot", "dashboard" })
+        {
+            var resource = GetResource(builder, resourceName);
+            var migrationWait = resource.Annotations
+                .OfType<WaitAnnotation>()
+                .Single(wait => ReferenceEquals(wait.Resource, migrationService));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(migrationWait.WaitType, Is.EqualTo(WaitType.WaitForCompletion));
+                Assert.That(migrationWait.ExitCode, Is.EqualTo(0));
+            });
+        }
+    }
+
+    private static IResource GetResource(IDistributedApplicationBuilder builder, string name) =>
+        builder.Resources.Single(resource => resource.Name == name);
+}
